20a6cfeb42704f10e30551bb06c2adaa3f226f85
wepe <weipforever@gmail.com>
2014-12-05 22:53:46 +0800

    add the demo of kNN algrithm

---
+++ kNN.py
@@ -0,0 +1,71 @@
+'''
+@author: wepon
+@github: https://github.com/wepe
+@blog:   http://blog.csdn.net/u012162613
+'''
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+from numpy import *
+import operator
+from os import listdir
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]                  
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet      
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)                  
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()            
+    classCount={}                                      
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)                              
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   
+    return normDataSet, ranges, minVals
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')          
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]                  
+        fileStr = fileNameStr.split('.')[0]                
+        classNumStr = int(fileStr.split('_')[0])          
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+     
+    testFileList = listdir('testDigits')       
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
