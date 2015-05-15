8fcd9bf9c5f3f0fd57450e0f1909a5d699e3fd33
Wepon <masterwepon@163.com>
2015-05-15 10:49:33 +0800

    add a method for loading RGB image

--- DeepLearning Tutorials/keras_usage/data.py
+++ DeepLearning Tutorials/keras_usage/data.py
@@ -10,7 +10,8 @@ import os
 from PIL import Image
 import numpy as np
 
-#读取文件夹mnist下的42000张图片，图片为灰度图，所以为1通道，如果是将彩色图作为输入,则将1替换为3,图像大小28*28
+#读取文件夹mnist下的42000张图片，图片为灰度图，所以为1通道，图像大小28*28
+#如果是将彩色图作为输入,则将1替换为3，并且data[i,:,:,:] = arr改为data[i,:,:,:] = [arr[:,:,0],arr[:,:,1],arr[:,:,2]]
 def load_data():
 	data = np.empty((42000,1,28,28),dtype="float32")
 	label = np.empty((42000,),dtype="uint8")
